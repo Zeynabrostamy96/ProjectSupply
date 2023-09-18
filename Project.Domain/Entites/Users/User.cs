7f48e1f11d1e8913b79af214371cbde88cb3b049
Zeynabrostamy96 <73117940+Zeynabrostamy96@users.noreply.github.com>
2023-09-18 12:23:55 -0700

    finally

--- Project.Domain/Entites/Users/User.cs
+++ Project.Domain/Entites/Users/User.cs
@@ -1,15 +1,14 @@
 ﻿using Project.Domain.Entites.Commons;
 using Project.Domain.Entites.Departments;
-using Project.Domain.Entites.Users;
 using System.Collections.Generic;
 
 namespace Project.Domain.Entites
 {
-    public class User: BaseEntity<long>
+    public class User : BaseEntity<long>
     {
         public string Name { get; set; }
         public string Email { get; set; }
         public List<Department> department { get; set; }
-        
+
     }
 }
